da6e65126974e921eacf48cea04e9815626af86a
MarzenaTomek MarzenaTomek <dowithdoggo@gmail.com>
2023-08-27 11:52:53 -0500

    Add 3 different ways of saying hallow to user

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,25 @@
 
 const prompt = require("prompt-sync")({ sigint: true });
-const name = prompt("what's your name? ");
 
 
-console.log(`hellow mr/mrs: ${name}`);
+
+let randomNumber = Math.floor(Math.random() * 3) // Math.random() => 0...1  like 0,01...0,01111
+let greeting = null;
+
+if (randomNumber == 0) {
+    greeting = "Aloha";
+}
+
+if (randomNumber == 1) {
+    greeting = "Hej";
+} 
+
+if (randomNumber == 2 ) {
+    greeting = "Hallo";
+  }
+
+
+
+//console.log(randomNumber);
+ const name = prompt("what's your name? ");
+ console.log(`${greeting} mr/mrs: ${name}`);
